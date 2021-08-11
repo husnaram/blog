cfb242ba6209b43f19bc1aa1b0db4b7d0c2f9430
Husna Ramadan <husna.ramadan@gmail.com>
2021-08-11 12:03:33 +0700

    Rebuild on Wed Aug 11 12:03:33 PM WIB 2021

--- themes/horlog/assets/css/postcss.config.js
+++ themes/horlog/assets/css/postcss.config.js
@@ -1,5 +1,14 @@
 const themeDir = __dirname + '/../../'
 
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    // see https://gohugo.io/hugo-pipes/postprocess/#css-purging-with-postcss
+    content: ['./hugo_stats.json'],
+    defaultExtractor: (content) => {
+        let els = JSON.parse(content).htmlElements
+        return els.tags.concat(els.classes, els.ids)
+    },
+})
+
 module.exports = {
     plugins: [
         require('postcss-import')({
@@ -8,6 +17,8 @@ module.exports = {
         require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'),
         require('autoprefixer')({
             path: [themeDir],
+            grid: true,
         }),
+        ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : []),
     ],
 }
